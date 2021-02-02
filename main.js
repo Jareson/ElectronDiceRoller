86466d8cd614abf26d5bd9ba649225805387a3e7
Jareson <jaresonv@outlook.com>
2021-02-01 22:04:56 -0600

    Turned off node integration, called jquery via html

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const electron = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
-let win  
+let win;
 
-function createWindow() { 
-   win = new BrowserWindow({
-      width: 275, 
-      height: 450, 
-      webPreferences: {
-         nodeIntegration: true
-      }
-   });
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true 
-   }));
-   win.setMenuBarVisibility(false);
-   win.setTitle("");
-}  
+async function createWindow() {
+  win = new BrowserWindow({
+    width: 275,
+    height: 450,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  win.setMenuBarVisibility(false);
+  win.setTitle("");
+}
 
-app.on('ready', createWindow) 
+app.on("ready", createWindow);
